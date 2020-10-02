5b723cfb00b6492dcf38a993d5610a3640452a40
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-10-02 20:58:56 +0530

    first commit

---
+++ biblechallenge/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer-copyright text-center py-3">
+
+            © 2020 Copyright: Maintained by  Kurosaki
+        </div>
+    )
+}
+
+export default Footer
